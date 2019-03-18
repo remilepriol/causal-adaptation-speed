d18d6fa0788803151ac04bd6b487c7cbc055b4d9
remilepriol <remi.lp.17@gmail.com>
2019-03-18 17:40:33 -0400

    Add script to plot big graph

--- script_categorical.py
+++ script_categorical.py
@@ -1,3 +1,4 @@
+import os
 import pickle
 
 import numpy as np
@@ -6,7 +7,7 @@ import categorical_distance
 
 if __name__ == "__main__":
 
-    n = 100
+    n = 300
     kk = np.arange(2, 100, 8)
 
     results = []
@@ -14,18 +15,23 @@ if __name__ == "__main__":
         for symmetric_init in [True, False]:
             for symmetric_intervention in [True, False]:
                 for concentration in [0.1, 1, 10]:
+                    distances = []
                     for k in kk:
-                        distances = categorical_distance.experiment(
-                            k, n, 1, intervention, symmetric_init, symmetric_intervention)
-                        exp = {
-                            'intervention': intervention,
-                            'symmetric_init': symmetric_init,
-                            'symmetric_intervention': symmetric_intervention,
-                            'concentration': concentration,
-                            'dimension': k,
-                            'distances': distances
-                        }
-                        results.append(exp)
+                        distances.append(categorical_distance.experiment(
+                            k, n, 1, intervention, symmetric_init, symmetric_intervention))
 
-    with open('results/categorical_distances.pkl', 'wb') as fout:
+                    exp = {
+                        'intervention': intervention,
+                        'symmetric_init': symmetric_init,
+                        'symmetric_intervention': symmetric_intervention,
+                        'concentration': concentration,
+                        'dimensions': kk,
+                        'distances': np.array(distances)
+                    }
+                    results.append(exp)
+
+    savedir = 'results'
+    os.makedirs(savedir, exist_ok=True)
+
+    with open(os.path.join(savedir, f'categorical_distances_{n}.pkl'), 'wb') as fout:
         pickle.dump(results, fout)
---
+++ script_plot.py
@@ -0,0 +1,69 @@
+import os
+import pickle
+import tqdm
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+def bigplot(exp):
+    """Draw 4 scatter plots and 2 line plots"""
+    causal_proba, anti_proba, causal_score, anti_score = tuple(
+        np.swapaxes(exp['distances'], 0, 1)
+    )
+    dimensions = exp['dimensions']
+
+    fig, axs = plt.subplots(nrows=3, ncols=2, figsize=(16, 20))
+
+    axs[0, 0].set_title('causal score vs proba')
+    axs[0, 1].set_title('anticausal score vs proba')
+    axs[1, 0].set_title('proba anticausal vs causal')
+    axs[1, 1].set_title('score anticausal vs causal')
+    axs[2, 0].set_title('proba ratio anticausal / causal ')
+    axs[2, 0].set_title('score ratio anticausal / causal ')
+
+    for k, cpd, apd, csd, asd in zip(
+            dimensions,
+            causal_proba, anti_proba,
+            causal_score, anti_score):
+        axs[0, 0].scatter(cpd, csd, label=k, alpha=.2)
+        axs[0, 1].scatter(apd, asd, label=k, alpha=.2)
+        axs[1, 0].scatter(cpd, apd, label=k, alpha=.2)
+        axs[1, 1].scatter(csd, asd, label=k, alpha=.2)
+
+    ratio_proba = anti_proba / causal_proba
+    ratio_score = anti_score / causal_score
+
+    confidence = (5,95)
+    for ax, ratio in zip([axs[2, 0], axs[2, 1]], [ratio_proba, ratio_score]):
+        ax.plot(dimensions, np.mean(ratio, axis=-1), label='mean')
+        ax.fill_between(dimensions, np.percentile(ratio,confidence[0],axis=-1),
+                               np.percentile(ratio,confidence[1],axis=-1),
+                        alpha=.4, label='confidence {} %'.format(confidence[1]-confidence[0]))
+
+    for ax in axs.flatten():
+        ax.legend()
+
+    return fig, axs
+
+
+def plotname(exp):
+    return "{}_{}_{}_{}.pdf".format(
+        exp['intervention'],
+        exp['concentration'],
+        exp['symmetric_init'],
+        exp['symmetric_intervention']
+    )
+
+
+if __name__ == "__main__":
+
+    with open('results/categorical_distances.pkl', 'rb') as fin:
+        results = pickle.load(fin)
+
+    plotdir = 'plots/categorical'
+    os.makedirs(plotdir, exist_ok=True)
+
+    for exp in tqdm.tqdm(results):
+        bigplot(exp)
+        plt.savefig(os.path.join(plotdir, plotname(exp)))
+        plt.close()
