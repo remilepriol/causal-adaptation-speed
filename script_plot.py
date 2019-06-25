71eb5661c0023415c1f3b7197080703f95586291
Rémi Le Priol <rlp@fb.com>
2019-06-25 16:35:50 -0400

    Plot convergence bound and convergence vs distance scatters

--- script_plot.py
+++ script_plot.py
@@ -76,17 +76,20 @@ def optim_plot():
     plotdir = 'plots/categorical_optim'
     os.makedirs(plotdir, exist_ok=True)
 
-    for exp in tqdm.tqdm(results):
-        longplot(exp, statistics=True)
-        plt.savefig(os.path.join(plotdir, 'average_'+longplotname(exp)))
-        longplot(exp, statistics=False)
-        plt.savefig(os.path.join(plotdir, 'curves_' + longplotname(exp)))
+    colors = {'causal': 'blue', 'anti': 'red'}
+    for exp in results:
+        print(longplotname(exp))
+        longplot(exp, colors, statistics=True, plot_bound=False)
+        plt.savefig(os.path.join(plotdir, 'average_' + longplotname(exp)))
+        # longplot(exp, colors, statistics=False)
+        # plt.savefig(os.path.join(plotdir, 'curves_' + longplotname(exp)))
+        optim_scatter(exp, colors)
+        plt.savefig(os.path.join(plotdir, 'scatter_' + longplotname(exp)))
         plt.close()
 
 
-def longplot(exp, confidence=(5, 95), statistics=True):
+def longplot(exp, colors, confidence=(5, 95), statistics=True, plot_bound=False):
     """Draw mean trajectory plot with percentiles"""
-    colors = {'causal':'blue', 'anti':'red'}
     fig, axs = plt.subplots(nrows=2, ncols=1, sharex=True, figsize=(10, 8))
     for ax, metric in zip(axs, ['kl', 'scoredist']):
         ax.grid(True)
@@ -98,7 +101,8 @@ def longplot(exp, confidence=(5, 95), statistics=True):
                 ax.plot(
                     exp['steps'],
                     values.mean(axis=1),
-                    label=model_family
+                    label=model_family,
+                    color=colors[model_family]
                 )
                 ax.fill_between(
                     exp['steps'],
@@ -106,7 +110,8 @@ def longplot(exp, confidence=(5, 95), statistics=True):
                     np.percentile(values, confidence[1], axis=1),
                     alpha=.4,
                     label='confidence {} %'.format(
-                        confidence[1] - confidence[0])
+                        confidence[1] - confidence[0]),
+                    color=colors[model_family]
                 )
             else:
                 ax.plot(
@@ -122,9 +127,94 @@ def longplot(exp, confidence=(5, 95), statistics=True):
         axs[0].legend()
     axs[1].set_ylabel('||transfer - model ||^2')
 
+    if plot_bound:
+        for model_family in ['causal', 'anti']:
+            initial_distance = exp['scoredist_' + model_family][0].mean()
+            steps = np.array(exp['steps'])[1:]
+            if exp['batch_size'] == 'full' and exp['scheduler_exponent'] == 0:
+                constant = rate_constant_fullbatch(
+                    smoothness=1 / 2,
+                    initial_lr=exp['lr'],
+                    initial_distance=initial_distance
+                )
+                bound = constant / (steps - 1)
+            elif np.isclose(exp['scheduler_exponent'], 2 / 3):
+                constant = rate_constant_twothird(
+                    smoothness=1 / 2,
+                    initial_lr=exp['lr'],
+                    initial_distance=initial_distance,
+                    variance=1 / exp['batch_size']
+                )
+                bound = constant / (steps ** (1 / 3) - 1)
+            else:
+                print('Convergence bound only available for '
+                      'gradient descent with constant learning rate '
+                      'or stochastic gradient descent with learning rate scheduling exponent 2/3.')
+                return
+
+            print(f"{model_family} initial distance = {initial_distance:.2f} \t"
+                  f" bound constant = {constant:.2f} \t ratio = {constant / initial_distance:.2f} \t"
+                  f"ratio constant / initial kl = {constant / exp['kl_' + model_family][0].mean():.2f}")
+
+            axs[0].plot(steps, bound, color=colors[model_family], linestyle='--')
+
 
 def longplotname(exp):
-    return "{intervention}_k={k}_lr={lr}_bs={batch_size}_concentration={concentration}_T={T}.pdf".format(**exp)
+    return ('{intervention}_k={k}_bs={batch_size}_rate={scheduler_exponent:.1f}'
+            '_lr={lr}_concentration={concentration}_T={T}.pdf').format(**exp)
+
+
+def rate_constant_twothird(smoothness, initial_lr, initial_distance, variance):
+    return (np.exp(3 * (2 * smoothness * initial_lr) ** 2)
+            * (1 + 4 * (smoothness * initial_lr) ** (3 / 2))
+            / (3 * initial_lr)
+            * (initial_distance + variance / smoothness ** 2))
+
+
+def rate_constant_fullbatch(smoothness, initial_lr, initial_distance):
+    return - initial_distance / (initial_lr * (smoothness / 2 * initial_lr - 1))
+
+
+def optim_scatter(exp, colors, end_step=1000):
+    fig, axs = plt.subplots(nrows=2, ncols=1, sharex=True, figsize=(10, 8))
+
+    index = np.searchsorted(exp['steps'], end_step)
+    initial_distances = {}
+    ends = {}
+    integrals = {}
+    for model_family in ['anti', 'causal']:
+        initial_distances[model_family] = exp['scoredist_' + model_family][0]
+        values = exp['kl_' + model_family]
+        ends[model_family] = values[index]
+        integrals[model_family] = values[:index].mean(axis=0)
+
+        axs[0].scatter(
+            initial_distances[model_family],
+            ends[model_family],
+            alpha=.3,
+            color=colors[model_family],
+            label=model_family
+        )
+        axs[1].scatter(
+            initial_distances[model_family],
+            integrals[model_family],
+            alpha=.3,
+            color=colors[model_family],
+            label=model_family
+        )
+
+    for ax,  metrics in zip(axs, [ends, integrals]):
+        ax.grid(True)
+        ax.set_yscale('log')
+        ax.set_xscale('log')
+        ax.plot(np.stack((initial_distances['causal'], initial_distances['anti']), axis=1).T,
+                np.stack((metrics['causal'], metrics['anti']), axis=1).T,
+                alpha=.1, color='black')
+
+    axs[0].set_ylabel(f'KL(transfer, model) at step {end_step}')
+    axs[0].legend()
+    axs[1].set_ylabel(f'Average KL(transfer, model) from step 0 to {end_step}')
+    axs[1].set_xlabel('Parameter squared distance from initialization to optimum.')
 
 
 if __name__ == "__main__":
