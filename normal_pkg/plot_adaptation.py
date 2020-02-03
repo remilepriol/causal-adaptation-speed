a1dffc02d03ec54b00d9ad04c48c57d6979640ae
remilepriol <remi.lp.17@gmail.com>
2020-02-02 22:24:18 -0500

    Plot learning curves

---
+++ normal_pkg/plot_adaptation.py
@@ -0,0 +1,24 @@
+import os
+import pickle
+from collections import defaultdict
+
+from categorical.sweep import two_plots
+
+
+def learning_curves(results_dir='normal_results'):
+    for k in [20]:
+        # Optimize hyperparameters for nsteps such that curves are k-invariant
+        nsteps = k ** 2 // 4
+        allresults = defaultdict(list)
+        init = 'natural'
+        for intervention in ['cause', 'effect']:
+            plotname = f'{intervention}_{init}_k={k}'
+            filepath = os.path.join(results_dir, plotname + '.pkl')
+            if os.path.isfile(filepath):
+                with open(filepath, 'rb') as fin:
+                    results = pickle.load(fin)
+                    two_plots(results, nsteps, plotname=plotname, dirname='normal')
+                    allresults[intervention] = results
+
+if __name__ == "__main__":
+    learning_curves()
